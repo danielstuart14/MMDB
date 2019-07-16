0a407b2a495f654634bfef5618acbf3e4882b922
Daniel Stuart <daniel.stuart14@gmail.com>
2019-07-16 16:07:30 -0300

    BranchDB: Viewer: Cleanup and fixes

--- src/viewer.py
+++ src/viewer.py
@@ -1,5 +1,5 @@
 import gi
-gi.require_version('Gtk', '3.0')
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 import branch
 import os
@@ -10,28 +10,31 @@ class TreeViewer:
         path = os.path.join(os.path.dirname(__file__), "viewer.xml")
         builder.add_from_file(path)
         self.db = None
-        self.column = None
-        self.listColumn = None
-        self.listmodel = None
-        self.listPathColumn = None
-        self.listPathModel = None
+        self.treeColumn = None
+        self.ValColumn = None
+        self.ObjColumn = None
+        self.TreeModel = None
+        self.ValModel = None
+        self.ObjModel = None
 
-        win = builder.get_object("AppWindow")
-        win.connect("destroy", Gtk.main_quit)
-        win.show_all()
+        self.appWin = builder.get_object("AppWindow")
+        self.appWin.connect("destroy", Gtk.main_quit)
+        self.appWin.show_all()
+        pos = self.appWin.get_position()
 
         self.conWin = builder.get_object("ConWindow")
-        self.errorWin = builder.get_object("ErrorWindow")
 
-        self.objWin = builder.get_object("ObjWindow")
-        self.objWin.connect('delete-event', self.closeObjView)
+        self.valWin = builder.get_object("ValWindow")
+        self.valWin.connect("delete-event", self.closeValView)
+        self.valWin.move(pos[0] + 420, pos[1])
         
-        self.pathWin = builder.get_object("PathWindow")
-        self.pathWin.connect('delete-event', self.closePathView)
+        self.objWin = builder.get_object("ObjWindow")
+        self.objWin.connect("delete-event", self.closeObjView)
+        self.objWin.move(pos[0] - 420, pos[1])
 
         self.tree = builder.get_object("TreeView")
-        self.list = builder.get_object("ListView")
-        self.pathList = builder.get_object("ListViewPath")
+        self.valList = builder.get_object("ValView")
+        self.objList = builder.get_object("ObjView")
         self.status = builder.get_object("statusIndicator")
 
         self.refreshButton = builder.get_object("refreshButton")
@@ -41,20 +44,16 @@ class TreeViewer:
         builder.get_object("ConnectButton").connect("clicked", self.openConnect)
         builder.get_object("ConApply").connect("clicked", self.conApply)
         builder.get_object("ConClose").connect("clicked", self.conClose)
-        builder.get_object("ErrorClose").connect("clicked", self.errorClose)
 
         self.urlBox = builder.get_object("urlbox")
         self.dbBox = builder.get_object("dbbox")
     
-    def errorClose(self, button=None):
-        self.errorWin.hide()
+    def closeValView(self, win, *data):
+        self.valWin.hide()
+        return True
     
     def closeObjView(self, win, *data):
         self.objWin.hide()
-        return True
-    
-    def closePathView(self, win, *data):
-        self.pathWin.hide()
         return True
 
     def conClose(self, button=None):
@@ -64,27 +63,27 @@ class TreeViewer:
         server = self.urlBox.get_text()
         database = self.dbBox.get_text()
         self.connect(server, database)
-        self.conClose()
 
     def connect(self, server, database):
         print("Trying to connect...")
         try:
             self.db = branch.connect(server, database, True)
-        except:
-            print("Error while connecting")
-            self.setStatusRed()
+        except Exception as e:
+            self.conClose()
+            self.setStatusRed("Error while connecting", str(e))
         else:
             print("Connected")
             self.refresh()
+            self.conClose()
 
     def refresh(self, button=None):
         try:
             tree = self.db.getStructure()
-            self.removeColumn()
+            if self.treeColumn != None:
+                self.treeModel.clear()
             self.jsonToTree(tree)
-        except:
-            print("Error while refreshing list")
-            self.setStatusRed()
+        except Exception as e:
+            self.setStatusRed("Error while refreshing list", str(e))
         else:
             print("List refreshed")
             self.setStatusGreen()
@@ -94,26 +93,32 @@ class TreeViewer:
         self.status.set_tooltip_text("Connected")
         self.refreshButton.set_sensitive(True)
 
-    def setStatusRed(self):
+    def setStatusRed(self, error, msg):
+        print(error)
+        print(msg)
         self.status.set_from_stock("gtk-no", -1)
         self.status.set_tooltip_text("Not connected")
         self.refreshButton.set_sensitive(False)
-        self.errorWin.show_all()
+
+        dialog = Gtk.MessageDialog(self.appWin,
+            Gtk.DialogFlags.MODAL,
+            Gtk.MessageType.ERROR,
+            Gtk.ButtonsType.CLOSE,
+            "An error has occurred!")
+        dialog.format_secondary_text(error)
+        dialog.run()
+        dialog.destroy()
 
     def openConnect(self, button):
         self.conWin.show_all()
-
-    def removeColumn(self):
-        if self.column != None:
-            self.model.clear()
     
-    def listViewObj(self, object, path, column):
-        if self.listPathColumn != None:
-            self.listPathModel.clear()
-        self.listPathModel = Gtk.ListStore(str)
-        itertree = self.model.get_iter(path)
+    def objView(self, object, path, column):
+        if self.ObjColumn != None:
+            self.ObjModel.clear()
+        self.ObjModel = Gtk.ListStore(str)
+        itertree = self.treeModel.get_iter(path)
         
-        if self.model.get_value(itertree, 0) == "/":
+        if self.treeModel.get_value(itertree, 0) == "/":
             obj_id = ""
             path = "/"
         else:
@@ -131,10 +136,9 @@ class TreeViewer:
                 children = list(self.db.getChildren(obj_id, path))
             else:
                 children = list(self.db.getObjects(path))
-        except:
-            print("Error while obtaining objects")
-            self.pathWin.hide()
-            self.setStatusRed()
+        except Exception as e:
+            self.objWin.hide()
+            self.setStatusRed("Error while obtaining objects", str(e))
             return
         
         if not(path.endswith("/")):
@@ -142,61 +146,52 @@ class TreeViewer:
         self.path = path + obj_id
         for obj in children:
             obj_id = str(obj["_id"])
-            self.listPathModel.append([obj_id])
+            self.ObjModel.append([obj_id])
 
-        self.pathList.set_model(self.listPathModel)
-        if self.listPathColumn == None:
+        self.objList.set_model(self.ObjModel)
+        if self.ObjColumn == None:
             render = Gtk.CellRendererText()
-            self.listPathColumn = Gtk.TreeViewColumn("Object IDs", render, text=0)
-            self.pathList.append_column(self.listPathColumn)
-            self.pathList.connect('row-activated', self.listView)
-        self.pathWin.show_all()
-        
+            self.ObjColumn = Gtk.TreeViewColumn("Object IDs", render, text=0)
+            self.objList.append_column(self.ObjColumn)
+            self.objList.connect("row-activated", self.valView)
+        self.objWin.show_all()
     
-    def listView(self, obj, path, column):
-        if self.listColumn != None:
-            self.listmodel.clear()
-        self.listmodel = Gtk.ListStore(str, str)
+    def valView(self, obj, path, column):
+        if self.ValColumn != None:
+            self.ValModel.clear()
+        self.ValModel = Gtk.ListStore(str)
         
-        iterpath = self.listPathModel.get_iter(path)
-        obj_id = self.listPathModel.get_value(iterpath, 0)
-        print(obj_id)
-        print(self.path)
+        iterpath = self.ObjModel.get_iter(path)
+        obj_id = self.ObjModel.get_value(iterpath, 0)
         
         try:
             json = self.db.readObject(obj_id, self.path)
-        except:
-            print("Error while obtaining object")
-            self.objWin.hide()
-            self.setStatusRed()
+        except Exception as e:
+            self.valWin.hide()
+            self.setStatusRed("Error while obtaining object", str(e))
             return
 
         for key in json:
             val = json[key]
             if not(isinstance(val, str)):
                 val = str(val)
-            self.listmodel.append([key, val])
+            self.ValModel.append([key + ": " + val])
 
-        self.list.set_model(self.listmodel)
-        if self.listColumn == None:
-            self.listColumn = Gtk.TreeViewColumn("Keys and Values")
-            key = Gtk.CellRendererText()
-            val = Gtk.CellRendererText()
-            self.listColumn.pack_start(key, True)
-            self.listColumn.pack_start(val, True)
-            self.listColumn.add_attribute(key, "text", 0)
-            self.listColumn.add_attribute(val, "text", 1)
-            self.list.append_column(self.listColumn)
-        self.objWin.show_all()
+        self.valList.set_model(self.ValModel)
+        if self.ValColumn == None:
+            render = Gtk.CellRendererText()
+            self.ValColumn = Gtk.TreeViewColumn("Values", render, text=0)
+            self.valList.append_column(self.ValColumn)
+        self.valWin.show_all()
 
     def createPath(self, path):
-        itertree = self.model.get_iter(path)
-        dbPath = self.model.get_value(itertree, 0)
+        itertree = self.treeModel.get_iter(path)
+        dbPath = self.treeModel.get_value(itertree, 0)
         while True:
             path.up()
             if str(path) != "":
-                itertree = self.model.get_iter(path)
-                upperPath = self.model.get_value(itertree, 0)
+                itertree = self.treeModel.get_iter(path)
+                upperPath = self.treeModel.get_value(itertree, 0)
                 if upperPath == "/":
                     dbPath = "/" + dbPath
                     break
@@ -204,19 +199,19 @@ class TreeViewer:
         return dbPath
 
     def jsonToTree(self, json):
-        self.model = Gtk.TreeStore(str)
+        self.treeModel = Gtk.TreeStore(str)
         root = self.appendModel("/")
         self.jsonToModel(json, root)
 
-        self.tree.set_model(self.model)
-        if self.column == None:
+        self.tree.set_model(self.treeModel)
+        if self.treeColumn == None:
             cellRenderer = Gtk.CellRendererText()
-            self.column = Gtk.TreeViewColumn("Paths", cellRenderer, text=0)
-            self.tree.append_column(self.column)
-            self.tree.connect('row-activated', self.listViewObj)
+            self.treeColumn = Gtk.TreeViewColumn("Paths", cellRenderer, text=0)
+            self.tree.append_column(self.treeColumn)
+            self.tree.connect("row-activated", self.objView)
 
     def appendModel(self, value, parent=None):
-        return self.model.append(parent,[value])
+        return self.treeModel.append(parent,[value])
 
     def jsonToModel(self, json, parent=None):
         for i in list(json):
